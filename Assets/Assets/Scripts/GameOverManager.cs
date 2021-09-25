a6eb50338ff7aa5cedb028c540dfb29b37ee2a92
Divya Kelaskar <divyakelaskar@gmail.com>
2021-09-25 10:25:24 +0530

    added the game files

---
+++ Assets/Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOverManager : MonoBehaviour
+{
+
+    public GameObject restartText; // Assign in inspector
+    public GameObject menuText;     // Assign in inspector
+
+    //  public GameObject Player;
+
+    void FixedUpdate ()
+	{
+
+        UIvisible();
+    }
+
+   
+	void Update ()
+	{
+
+
+        playerDestroyed();
+        
+	}
+
+    void playerDestroyed() {
+
+        GameObject[] player;
+        player = GameObject.FindGameObjectsWithTag("Player");
+
+        if (player.Length == 0 && Input.GetKeyDown(KeyCode.Escape))         // Checks if the player is not present and if the Escape key is pressed, it will load the Menu Scene
+        {
+            SceneManager.LoadScene("Menu");
+        }
+        else if (player.Length == 0 && Input.GetKeyDown(KeyCode.R))         //Checks if the player is not present and if the R key is pressed, it will load the GamePlay Scene i.e S1 from beginning
+        {
+            SceneManager.LoadScene("S1");
+        }
+
+        
+
+    }
+    void UIvisible() {
+
+
+        GameObject[] player;
+        player = GameObject.FindGameObjectsWithTag("Player");               
+        if (player.Length == 0)
+        {
+            
+            restartText.SetActive(true);            //Sets the Text Visible from Canvas
+            menuText.SetActive(true);                //Sets the Text Visible from Canvas
+        }
+    }
+   
+
+   
+}
---
+++ Assets/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;                          //This needs to added
+
+public class MainMenu : MonoBehaviour {
+
+	
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");                  //Will Load the Main Gameplay Scene
+
+    }
+    public void QuitGame()
+	{
+		Application.Quit ();                            //Will Exit the Game
+	}
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
